d5c91145b6032681d5fb52778f532cfdcd480efc
obongsik <zkfkdzhdo@naver.com>
2024-09-30 23:38:50 +0900

    feat :sequelize install

---
+++ mysql.js
@@ -0,0 +1,19 @@
+require('dotenv').config(); // 환경 변수 불러오기
+const mysql = require('mysql2/promise');
+
+let test = async () => {
+    // 환경 변수 접근
+    const db = mysql.createPool({
+        host: process.env.DB_HOST,
+        user: process.env.DB_USER,
+        password: process.env.DB_PW,
+        port: process.env.DB_PORT,
+        database: process.env.DB_NAME,
+        waitForConnections: true,
+        insecureAuth: true
+    });
+    let sql = 'SELECT * FROM reservation';
+    let [rows, fields] = await db.query(sql);
+    console.log(rows);
+};
+test();
