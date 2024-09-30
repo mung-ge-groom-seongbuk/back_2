e4b073f1cd636f6912930be38af9489a5de017bf
obongsik <zkfkdzhdo@naver.com>
2024-10-01 00:55:35 +0900

    fix : 오타

--- config/config.js
+++ config/config.js
@@ -2,14 +2,14 @@ require('dotenv').config();
 module.exports = {
     development: {
         username: process.env.DB_USER,
-        password: process.env.DB_PW,
+        password: process.env.DB_PASSWORD,
         database: process.env.DB_NAME,
         host: process.env.DB_HOST,
         dialect: "mysql"
     },
     production: {
         username: process.env.DB_USER,
-        password: process.env.DB_PW,
+        password: process.env.DB_PASSWORD,
         database: process.env.DB_NAME,
         host: process.env.DB_HOST,
         dialect: "mysql",
