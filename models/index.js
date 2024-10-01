17081402dd424f7c1b9c6525935432815221e4b4
obongsik <zkfkdzhdo@naver.com>
2024-10-02 00:31:10 +0900

    fix ; 일단 원래대로

--- models/index.js
+++ models/index.js
@@ -5,16 +5,17 @@ const path = require('path');
 const Sequelize = require('sequelize');
 const process = require('process');
 const basename = path.basename(__filename);
+const env = process.env.NODE_ENV || 'development';
+const config = require(__dirname + '/../config/config.json')[env];
 const db = {};
 
-// 환경 변수에서 데이터베이스 정보 가져오기
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  dialect: 'mysql'
-});
+let sequelize=new Sequelize(config.database, config.username, config.password, config);
+if (config.use_env_variable) {
+  sequelize = new Sequelize(process.env[config.use_env_variable], config);
+} else {
+  sequelize = new Sequelize(config.database, config.username, config.password, config);
+}
 
-// 모델 파일 읽기 및 등록
 fs
   .readdirSync(__dirname)
   .filter(file => {
@@ -30,7 +31,6 @@ fs
     db[model.name] = model;
   });
 
-// 모델 간의 관계 정의 (Associations)
 Object.keys(db).forEach(modelName => {
   if (db[modelName].associate) {
     db[modelName].associate(db);
