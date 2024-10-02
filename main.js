de74489ca6b5ad8065d5ee7afc9300331b93e618
obongsik <zkfkdzhdo@naver.com>
2024-10-02 18:56:39 +0900

    fe

--- main.js
+++ main.js
@@ -16,6 +16,9 @@ app.use(bodyParser.json());
 app.use(session({ secret: process.env.SESSION_SECRET, resave: false, saveUninitialized: true }));
 app.use(flash());
 
+
+
+
 // 기본 경로에 대한 라우트 추가
 app.get('/', (req, res) => {
     res.send('Hello, World!');
