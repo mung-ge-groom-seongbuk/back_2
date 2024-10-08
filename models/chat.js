f5d7d9aaa16cfae68c4e22b705aaa7efe9b19da6
obongsik <zkfkdzhdo@naver.com>
2024-10-09 01:36:30 +0900

    fix

--- models/chat.js
+++ models/chat.js
@@ -1,4 +1,4 @@
-//chat table
+// chat table
 
 const { DataTypes } = require("sequelize");
 
@@ -14,7 +14,7 @@ module.exports = (sequelize) => {
             type: DataTypes.INTEGER,
             allowNull: false,
             references: { // 외래 키 설정 (발신자)
-                model: "user", // 참조할 테이블
+                model: "User", // 참조할 테이블 (모델 이름은 대문자로 시작해야 합니다)
                 key: "user_id" // 참조할 테이블의 필드
             }
         },
@@ -22,7 +22,7 @@ module.exports = (sequelize) => {
             type: DataTypes.INTEGER,
             allowNull: false,
             references: { // 외래 키 설정 (수신자)
-                model: "user", // 참조할 테이블
+                model: "User", // 참조할 테이블 (모델 이름은 대문자로 시작해야 합니다)
                 key: "user_id" // 참조할 테이블의 필드
             }
         },
@@ -35,10 +35,17 @@ module.exports = (sequelize) => {
             allowNull: false,
             defaultValue: DataTypes.NOW // 기본값: CURRENT_TIMESTAMP
         }
-    },
-    {
+    }, {
         tableName: "chat", // 테이블명 설정
         timestamps: false // createdAt, updatedAt 자동 생성 방지
     });
+
+    // 관계 정의
+    Chat.associate = (models) => {
+        Chat.belongsTo(models.User, { foreignKey: 'sender_id', as: 'sender' });
+        Chat.belongsTo(models.User, { foreignKey: 'receiver_id', as: 'receiver' });
+    };
+
     return Chat;
-}
+};
+
