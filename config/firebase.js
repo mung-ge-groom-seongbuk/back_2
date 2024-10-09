671a84d737bedd39938cdf555292a574b6889f34
obongsik <zkfkdzhdo@naver.com>
2024-10-09 20:37:14 +0900

    fix

--- config/firebase.js
+++ config/firebase.js
@@ -2,7 +2,7 @@
 const admin = require('firebase-admin');
 
 // Firebase Admin SDK 서비스 계정 키 경로
-const serviceAccount = require('./serviceAccountKey.json'); // config 폴더 내에 있는 경우, 경로 수정
+const serviceAccount = require('./config/serviceAccountKey.json'); // config 폴더 내에 있는 경우, 경로 수정
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
@@ -10,13 +10,21 @@ admin.initializeApp({
 
 const sendFirebaseNotification = async (registrationToken, payload) => {
     try {
-        await admin.messaging().sendToDevice(registrationToken, payload);
-        console.log('Notification sent successfully');
+        // V1 API 사용
+        const response = await admin.messaging().send({
+            token: registrationToken,
+            notification: {
+                title: payload.notification.title,
+                body: payload.notification.body,
+            },
+        });
+        console.log('Notification sent successfully:', response);
     } catch (error) {
         console.error('Error sending notification:', error);
     }
 };
 
 module.exports = { sendFirebaseNotification };
+
 
 
